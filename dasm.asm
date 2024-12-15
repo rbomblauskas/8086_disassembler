a0e0e83fe9f37120209bc6b5cc246c957656fe2f
Rokas <rbomblauskas@gmail.com>
2024-12-15 02:00:17 +0200

    Dabar programa skaito komandines eilutes argumentus, ir is failo

--- dasm.asm
+++ dasm.asm
@@ -1,4 +1,5 @@
-;Kodo stilius
+;Disasemblerio autorius: Rokas Bomblauskas, PS 1 k. 1 gr.
+;Kodo stilius:
 ;Žymės: SCREAMING_SNAKE_CASE
 ;Procedūros: camelCase
 ;Kintamieji: camelCase
@@ -13,13 +14,19 @@ opLentDydis   EQU 5000
 grpPoslinkis  EQU 4096
 
 .data
-  masKodas  db 0Eh, 24h, 24h, 80h, 06h, 10h, 02h, 30h, 8ch, 0c8h, 0b4h, 09h, 0bah, 0deh, 01h, 0cdh, 21h, 0b4h, 0ah, 0bah, 63h, 01h, 0cdh, 21h, 0b4h, 09h, 0bah, 11h, 02h
+  masKodas  db 81h, 0BFh, 05h, 01h, 04h, 08h, 8Ah, 0CFh, 0Eh, 24h, 24h, 80h, 06h, 10h, 02h, 30h, 8ch, 0c8h, 0b4h, 09h, 0bah, 0deh, 01h, 0cdh, 21h, 0b4h, 0ah, 0bah, 63h, 01h, 0cdh, 21h, 0b4h, 09h, 0bah, 11h, 02h
+  
   skBuf     db skBufDydis dup ('$')
   rBuf      db rBufDydis dup ('$')
-
-  lentVard  db "op.bin", 0
   opLent    db opLentDydis dup (?)
+
   lFail     dw ?
+  dFail     dw ?
+  rFail     dw ?
+
+  lVard     db "op.bin", 0
+  dVard     db vardoBufDydis dup(0)
+  rVard     db vardoBufDydis dup(0)
 
   instruk   dw ?
 
@@ -31,29 +38,62 @@ grpPoslinkis  EQU 4096
   rmf  dw 0
   
   arDekModRM db 0
-  dekInstruk db 5
+  dekInstruk db 255
+
+  dekBaitai    db 10 dup(?)
+  dekBaituSkc  db 0 
+  dekBaitaiHex db 20 dup(?)
+
+  nuskBaituSkc db skBufDydis
 
   rmLent    db "BX+SI$BX+DI$BP+SI$BP+DI$SI$$$$DI$$$$BP$$$$BX$"
-	regLent0  db "AL$CL$DL$BL$AH$CH$DH$BH$"
+  regLent0  db "AL$CL$DL$BL$AH$CH$DH$BH$"
   regLent1  db "AX$CX$DX$BX$SP$BP$SI$DI$"
   regLent2  db "ES$CS$SS$DS$"
   
-
   klZin     db "Klaida", 10, 13, "$"
   naujaEil  db 10, 13, "$"
+  pagZin    db "Sveiki, cia pagalbos pranesimas", 10, 13, "$"
+  neatpaz   db "NEATPAZINTA$"
   
 
 .code
   PRADZIA:
   MOV ax, @data
   MOV ds, ax
-  
-  SKAITYMAS:
+
+  MOV ch, 0
+  MOV cl, [es:0080h]
+  CMP cx, 0
+  JE PAGALBA
+  MOV bx, 0081h
+
+  KLAUSTUKO_PAIESKA:
+  CMP  [es:bx], '?/'
+  JE   PAGALBA
+  INC  bx
+  LOOP KLAUSTUKO_PAIESKA
+
+  ARGUMENTU_SKAITYMAS:
+  MOV  bx, 0081h
+  MOV  cl, [es:0080h]
+  CALL praleistiTarpusKomEil
+  MOV  di, offset dVard
+  CALL skaitytiVardaKomEil
+  CMP  cx, 0
+  JE   PAGALBA
+  CALL praleistiTarpusKomEil
+  MOV  di, offset rVard
+  CALL skaitytiVardaKomEil
+  CMP  cx, 0
+  JNE  PAGALBA
+
+  OPKODU_LENTELES_SKAITYMAS:
   MOV ah, 3Dh
   MOV al, 00
-  MOV dx, offset lentVard
+  MOV dx, offset lVard
   INT 21h
-  JC  klaida
+  JC  KLAIDA
   MOV lFail, ax
   
   MOV bx, lFail
@@ -63,21 +103,54 @@ grpPoslinkis  EQU 4096
   INT	21h
   JC	KLAIDA
 
-  MOV si, offset masKodas
+  MOV ah, 3Eh
+  MOV bx, lFail
+  INT 21h
+  JC  KLAIDA
+
+  DUOMENU_FAILO_ATIDARYMAS:
+  MOV ah, 3Dh
+  MOV al, 00
+  MOV dx, offset dVard
+  INT 21h
+  JC  KLAIDA
+  MOV dFail, ax
+
+  JMP INSTRUKCIJU_DEKODAVIMAS
+  
+  ;REZULTATU_FAILO_SUKURIMAS:
+  ;MOV ah, 3Ch
+  ;MOV al, 00
+  ;MOV dx, offset rVard
+  ;INT 21h
+  ;JC  KLAIDA
+  ;MOV rFail, ax
+  ;JMP INSTRUKCIJU_DEKODAVIMAS
+
+  PAGALBA:
+  MOV dx, offset pagZin
+  CALL spausdintiEilute
+  JMP PABAIGA
+
+  ;MOV si, offset masKodas
   
   INSTRUKCIJU_DEKODAVIMAS:
-  MOV di, offset rBuf
+  ;INT 3h
+  MOV dekBaituSkc, 0
   MOV arDekModRM, 0
+  MOV di, offset rBuf
   CALL dekoduotiInstrukcija
+
+  CALL spausdintiDekoduotusBaitus
   MOV dx, offset rBuf
   CALL spausdintiEilute
-  
-  DEC dekInstruk
-  CMP dekInstruk, 0
-  JNE INSTRUKCIJU_DEKODAVIMAS
 
-
+  JMP INSTRUKCIJU_DEKODAVIMAS
   
+  ;DEC dekInstruk
+  ;CMP dekInstruk, 0
+  ;JNE INSTRUKCIJU_DEKODAVIMAS
+
   PABAIGA:
   MOV ah, 4Ch
   MOV al, 0
@@ -88,16 +161,61 @@ grpPoslinkis  EQU 4096
   MOV dx, offset klZin
   INT 21h
   JMP PABAIGA
+
+PROC skaitytiIsFailo
+  PUSH bx
+  PUSH cx
+  PUSH dx
+  MOV bx, dFail
+  MOV ah, 3Fh
+  MOV cx, skBufDydis
+  MOV dx, offset skBuf
+  INT 21h
+  JC KLAIDA_SKAITANT
+  SKAITYTI_IS_FAILO_PABAIGA:
+  POP	dx
+  POP	cx
+  POP bx
+  RET
+  KLAIDA_SKAITANT:
+  MOV ax, 0
+  JMP SKAITYTI_IS_FAILO_PABAIGA
+skaitytiIsFailo ENDP
   
 PROC skaitytiBaita
+  PUSH bx
   ;I AL IDEDAMAS SEKANTIS BAITAS
-  LODSB ;; TODO: po to padaryti labiau advanced.
+  CMP nuskBaituSkc, skBufDydis
+  JB PRALEISTI_BUFERIO_NUSKAITYMA
+  CALL skaitytiIsFailo
+  CMP ax, 0
+  JE FAILO_PABAIGA
+  MOV nuskBaituSkc, 0
+  MOV si, offset skBuf
+  PRALEISTI_BUFERIO_NUSKAITYMA:
+  LODSB
+  MOV bx, offset dekBaitai
+  MOV dx, 0
+  MOV dl, dekBaituSkc
+  ADD bx, dx
+  MOV byte ptr[bx], al
+  INC dekBaituSkc
+  INC nuskBaituSkc
+  POP bx
   RET
+  FAILO_PABAIGA:
+  JMP PABAIGA
 ENDP skaitytiBaita
 
 PROC skaitytiZodi
   ;I AX IDEDAMI SEKANTYS DU BAITAI
-  LODSW
+  PUSH bx
+  call skaitytiBaita
+  MOV bl, al
+  call skaitytiBaita
+  MOV ah, al
+  MOV al, bl
+  POP bx
   RET
 ENDP skaitytiZodi
 
@@ -114,8 +232,10 @@ PROC dekoduotiInstrukcija
   ADD bx, ax
   MOV instruk, bx
   
+  CMP byte ptr[bx], "-"
+  JE NEATPAZINTA_INSTRUKCIJA
   CMP byte ptr[bx], "9"
-  ja NERA_GRUPEJE
+  JA NERA_GRUPEJE
   CALL dekoduotiModRM
   MOV ah, 0
   MOV al, byte ptr[bx]
@@ -124,11 +244,10 @@ PROC dekoduotiInstrukcija
   ;Galbut naudoti 1 baito modrm laukus?
   ADD ax, regf
   SHL ax, 4 ; Dauginame is 16
-  
   MOV bx, offset opLent
   ADD bx, grpPoslinkis
   ADD bx, ax
-
+  CMP byte ptr[bx], "-"
   NERA_GRUPEJE:
   CALL rasytiIkiTarpo
   CMP byte ptr[bx], " "
@@ -136,24 +255,27 @@ PROC dekoduotiInstrukcija
   MOV bx, instruk
   CALL praleistiArgumenta
   ARGUMENTU_DEKODAVIMAS:
-
   CALL rasytiTarpa
   CALL dekoduotiArgumenta
   CALL praleistiArgumenta
   CMP byte ptr[bx], " "
-  JE PRALEISTI_ANTRA_ARGUMENTA
+  JE INSTRUKCIJOS_DEKODAVIMO_PABAIGA
   MOV dx, ","
   CALL rasytiSimboli
   CALL rasytiTarpa
   CALL dekoduotiArgumenta
-  PRALEISTI_ANTRA_ARGUMENTA:
+
+  INSTRUKCIJOS_DEKODAVIMO_PABAIGA:
   MOV byte ptr [di], "$"
-
-
   POP cx
   POP bx
   POP ax
   RET
+
+  NEATPAZINTA_INSTRUKCIJA:
+  MOV dx, offset neatpaz
+  CALL rasytiIkiDolerio
+  JMP INSTRUKCIJOS_DEKODAVIMO_PABAIGA
 ENDP dekoduotiInstrukcija
 
 PROC dekoduotiArgumenta
@@ -276,6 +398,7 @@ PROC dekoduotiArgumenta
   NEPRASIDEDA_E_DIDZIAJA:
   CMP byte ptr[bx], "G"
   JNE NEPRASIDEDA_G
+  CALL dekoduotiModRM
   mov ax, regf
   mov dl, 3
   mul dl
@@ -337,17 +460,16 @@ PROC dekoduotiModRM
   CALL skaitytiBaita
   mov ah, 0
   MOV modf, ax
-	SHR modf, 6h
-	MOV regf, ax
-	SHR regf, 3h
-	AND regf, 7h
-	MOV rmf, ax
-	AND rmf, 7h
+  SHR modf, 6h
+  MOV regf, ax
+  SHR regf, 3h
+  AND regf, 7h
+  MOV rmf, ax
+  AND rmf, 7h
 
   MOV arDekModRM, 1
 
   MODRM_DEKODAVIMO_PABAIGA:
-
   POP ax
   RET
 ENDP dekoduotiModRM
@@ -375,14 +497,14 @@ PROC spausdinti16Baitu
 ENDP spausdinti16Baitu
 
 PROC spausdintiEilute
-	PUSH ax
+  PUSH ax
   PUSH dx
-	MOV ah, 09
-	INT 21h
+  MOV ah, 09
+  INT 21h
   MOV dx, offset naujaEil
   INT 21h
   POP dx
-	POP ax
+  POP ax
   RET
 ENDP spausdintiEilute
 
@@ -444,57 +566,104 @@ PROC praleistiArgumenta
 ENDP praleistiArgumenta
 
 PROC rasytiAX
-	push ax
-	mov al, ah
-	call rasytiAL
-	pop ax
-	call rasytiAL
+  push ax
+  mov al, ah
+  call rasytiAL
+  pop ax
+  call rasytiAL
   RET
 ENDP rasytiAX
 
 
 PROC rasytiAL
-	push ax
-	push cx
-	push ax
-	mov cl, 4
-	shr al, cl
-	call rasytiHex
-	pop ax
-	call rasytiHex
-	pop cx
-	pop ax
+  push ax
+  push cx
+  push ax
+  mov cl, 4
+  shr al, cl
+  call rasytiHex
+  pop ax
+  call rasytiHex
+  pop cx
+  pop ax
   RET
 ENDP rasytiAL
 
 PROC rasytiHex
-	push ax
-	push dx
-	
-	and al, 0Fh ;nunulinam vyresniji pusbaiti AND al, 00001111b
-	cmp al, 9
-	jbe PrintHexSkaitmuo_0_9
-	jmp PrintHexSkaitmuo_A_F
-	
-	PrintHexSkaitmuo_A_F: 
-	sub al, 10 ;10-15 ===> 0-5
-	add al, 41h
+  push ax
+  push dx
+  
+  and al, 0Fh ;nunulinam vyresniji pusbaiti AND al, 00001111b
+  cmp al, 9
+  jbe PrintHexSkaitmuo_0_9
+  jmp PrintHexSkaitmuo_A_F
+  
+  PrintHexSkaitmuo_A_F: 
+  sub al, 10 ;10-15 ===> 0-5
+  add al, 41h
   mov [di], al
   inc di
-	jmp PrintHexSkaitmuo_grizti
-	
-	
-	PrintHexSkaitmuo_0_9: ;0-9
-	add al, 30h
-	mov [di], al
+  jmp PrintHexSkaitmuo_grizti
+  
+  
+  PrintHexSkaitmuo_0_9: ;0-9
+  add al, 30h
+  mov [di], al
   inc di
-	jmp printHexSkaitmuo_grizti
-	
-	printHexSkaitmuo_grizti:
-	pop dx
-	pop ax
+  jmp printHexSkaitmuo_grizti
+  
+  printHexSkaitmuo_grizti:
+  pop dx
+  pop ax
   RET
 ENDP rasytiHex
 
+praleistiTarpusKomEil PROC
+  PRALEISTI_TARPA:
+  CMP  byte ptr [es:bx], ' '
+  JNE  PRALEISTI_TARPUS_PABAIGA
+  INC  bx
+  LOOP PRALEISTI_TARPA
+  PRALEISTI_TARPUS_PABAIGA:
+  RET
+praleistiTarpusKomEil ENDP
+
+skaitytiVardaKomEil PROC
+  VARDO_SKAITYMAS:
+  MOV al, [es:bx]
+  MOV [di], al
+  INC di
+  INC bx
+  DEC cx
+  JZ  VARDO_SKAITYMO_PABAIGA
+  CMP byte ptr [es:bx], ' '
+  JNE VARDO_SKAITYMAS
+  VARDO_SKAITYMO_PABAIGA:
+  MOV byte ptr[di], 0
+  RET
+skaitytiVardaKomEil ENDP
+
+spausdintiDekoduotusBaitus PROC
+  PUSH cx
+  PUSH si
+  PUSH di
+
+  MOV ch, 0
+  MOV cl, dekBaituSkc
+  MOV si, offset dekBaitai
+  MOV di, offset dekBaitaiHex
+  BAITU_KODAVIMO_CIKLAS:
+  MOV al, [si]
+  CALL rasytiAL
+  INC si
+  LOOP BAITU_KODAVIMO_CIKLAS
+  MOV byte ptr[di], "$"
+
+  POP di
+  POP si
+  POP cx
+
+  RET
+spausdintiDekoduotusBaitus ENDP
 
 END PRADZIA
